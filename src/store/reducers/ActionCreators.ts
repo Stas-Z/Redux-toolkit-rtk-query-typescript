eddcdedc21c9e86fa0905a0b636377ab6990e73a
Stas-Z <stanislav@zettafoods.ru>
2023-03-16 18:43:14 +0200

    extraReducers

--- src/store/reducers/ActionCreators.ts
+++ src/store/reducers/ActionCreators.ts
@@ -1,3 +1,4 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { IUser } from "../models/IUser";
 import { AppDispatch } from "../store";
@@ -8,14 +9,28 @@ function getErrorMessage(error: unknown) {
   return String(error);
 }
 
-export const fetchUsers = () => async (dispatch: AppDispatch) => {
-  try {
-    dispatch(userSlice.actions.usersFetching());
-    const response = await axios.get<IUser[]>(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    dispatch(userSlice.actions.usersFetchingSucces(response.data));
-  } catch (e) {
-    dispatch(userSlice.actions.usersFetchingError(getErrorMessage(e)));
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+//   try {
+//     dispatch(userSlice.actions.usersFetching());
+//     const response = await axios.get<IUser[]>(
+//       "https://jsonplaceholder.typicode.com/users"
+//     );
+//     dispatch(userSlice.actions.usersFetchingSucces(response.data));
+//   } catch (e) {
+//     dispatch(userSlice.actions.usersFetchingError(getErrorMessage(e)));
+//   }
+// };
+
+export const fetchUsers = createAsyncThunk(
+  "user/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get<IUser[]>(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(getErrorMessage(e));
+    }
   }
-};
+);
--- src/store/reducers/UserSlice.ts
+++ src/store/reducers/UserSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { IUser } from "../models/IUser";
+import { fetchUsers } from "./ActionCreators";
 
 interface UserState {
   users: IUser[];
@@ -17,15 +18,29 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    usersFetching(state) {
-      state.isLoading = true;
-    },
-    usersFetchingSucces(state, action: PayloadAction<IUser[]>) {
+    // usersFetching(state) {
+    //   state.isLoading = true;
+    // },
+    // usersFetchingSucces(state, action: PayloadAction<IUser[]>) {
+    //   state.isLoading = false;
+    //   state.error = "";
+    //   state.users = action.payload;
+    // },
+    // usersFetchingError(state, action: PayloadAction<string>) {
+    //   state.isLoading = false;
+    //   state.error = action.payload;
+    // },
+  },
+  extraReducers: {
+    [fetchUsers.fulfilled.type]: (state, action: PayloadAction<IUser[]>) => {
       state.isLoading = false;
       state.error = "";
       state.users = action.payload;
     },
-    usersFetchingError(state, action: PayloadAction<string>) {
+    [fetchUsers.pending.type]: (state) => {
+      state.isLoading = true;
+    },
+    [fetchUsers.rejected.type]: (state, action: PayloadAction<string>) => {
       state.isLoading = false;
       state.error = action.payload;
     },
